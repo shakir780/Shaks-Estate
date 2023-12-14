311c31ddffc1078d577da3c7e16ac95fae3812f9
shakir.kamal <shakir.kamal@kendimed.com>
2023-12-14 20:03:55 +0200

    Connected to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
