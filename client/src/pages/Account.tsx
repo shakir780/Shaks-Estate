d454c90b1cd5a30e5744b90640abbbe566c2d07f
shakir.kamal <shakir.kamal@kendimed.com>
2023-12-24 01:29:21 +0200

    Created visualization for Acoount Page

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-type Props = {};
+const Account = () => {
+  const fileRef = useRef<HTMLInputElement | null>(null);
 
-const Account = (props: Props) => {
-  return <div>Account</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center font-bold py-6 text-3xl">Personal Info</h1>
+      <form className="flex flex-col gap-4">
+        <input ref={fileRef} type="file" className="hidden" accept="img/*" />
+        <img
+          onClick={() => fileRef.current && fileRef.current.click()}
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+          src=""
+          alt=""
+        />
+        <input className="border p-3 rounded-lg" type="username" />
+        <input className="border p-3 rounded-lg" type="email" />
+        <input className="border p-3 rounded-lg" type="password" />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer font-bold">
+          Deactivate Account
+        </span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
